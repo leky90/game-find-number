ce1692f718f00f5a238c614a5bc1d6ca03fd668e
Ky Le <ldky90@gmail.com>
2022-08-14 15:58:03 +0700

    initial commit

---
+++ apps/game-shot-ball/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'game-shot-ball',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/game-shot-ball',
+};
